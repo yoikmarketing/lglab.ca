a49f36dbfdd7190a08bcc5d812c19c38cd42ce53
Mateja Lasan <m.vuk.lasan@icloud.com>
2021-03-03 17:46:31 -0500

    Initialize CMS and add gallery image model

---
+++ content/gallery/example-image.md
@@ -0,0 +1,6 @@
+---
+title: Example Image
+description: This image is an example
+alt: An example
+image: /images/uploads/example.png
+---
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../styles/index.css'
 
 function Website({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default Website
