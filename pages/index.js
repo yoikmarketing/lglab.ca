cd7243a3ebe2c2d4fd73974c7e863ee91acd3896
Ben Durston <bengdurston@gmail.com>
2021-04-01 17:40:38 -0400

    Added landing component.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import Landing from "../components/landing"
 import Content from "../components/content"
 
 export default function Home() {
   return (
-    <Content>
-      <h1 className="text-5xl text-center text-gray-700 dark:text-gray-300">
-        Next.js Website Template
-      </h1>
-    </Content>
+    <div>
+      <Landing />
+      <Content>
+        <h4 className="text-5xl text-center text-gray-700 dark:text-gray-300">
+          More information Section
+        </h4>
+      </Content>
+    </div>
+    
   )
 }
