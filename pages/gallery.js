552830d979050be07349ba952882bad827dc410a
Ben Durston <bengdurston@gmail.com>
2021-04-11 13:48:19 -0400

    Added styling to the members page.

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,4 +1,5 @@
 import Post from '../components/post'
+import Content from "../components/content"
 import { getAllGalleryImages } from '../lib/gallery'
 
 export async function getStaticProps() {
@@ -12,18 +13,20 @@ export async function getStaticProps() {
 
 export default function Gallery({ galleryImages }) {
   return(
-    <div>
-      {galleryImages.map((post) => (
-        <Post 
-          key = {post.title}
-          title = {post.title}
-          description = {post.description}
-          image = {post.image}
-          alt = {post.alt}
-          published = {post.published}
-        />
-      ))}
-    </div>
+    <Content>
+      <div className="p-4 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {galleryImages.map((post) => (
+          <Post 
+            key = {post.title}
+            title = {post.title}
+            description = {post.description}
+            image = {post.image}
+            alt = {post.alt}
+            published = {post.published}
+          />
+        ))}
+      </div>
+    </Content>
   )
 }
 
