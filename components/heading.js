a0745d138479b20b422c67c18be3d3215de70ff3
Mateja Lasan <m.vuk.lasan@icloud.com>
2021-08-02 00:04:13 -0400

    Modularize styles for heading component

--- components/heading.js
+++ components/heading.js
@@ -1,19 +1,19 @@
+import styles from "@styles/heading.module.css"
+
 export default function Heading({ title, lead }) {
   return (
-    <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h1 className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            {title || "Take control of your team."}
-          </h1>
-          {lead ? (
-            <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-              {lead}
-            </p>
-          ) : (
-            <></>
-          )}
-        </div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          {title || "Take control of your team."}
+        </h1>
+        {lead ? (
+          <p className={styles.lead}>
+            {lead}
+          </p>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   )
